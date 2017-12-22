6d0ab3b7e943da946e1cb9ab7bff8c6df5c12c59
Gmugra <alexei_khatskevich@pisem.net>
2017-12-22 21:13:35 +0100

    fixed failed git commit

--- src/main/java/net/cactusthorn/switches/rules/Switches.java
+++ src/main/java/net/cactusthorn/switches/rules/Switches.java
@@ -1,61 +1,10 @@
 package net.cactusthorn.switches.rules;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
 import net.cactusthorn.switches.SwitchParameter;
 
-@XmlRootElement(name = "switches")
-@XmlAccessorType(XmlAccessType.NONE)
-public class Switches {
+public interface Switches {
 
-	private Switches() {}
+	boolean exists(final String switchName );
 	
-	@XmlElement(name = "switch")
-	private List<Switch> switches;
-	
-	public boolean exists(final String switchName ) {
-		return switches.stream().anyMatch(s -> switchName.equals(s.name()));
-	}
-	
-	public boolean turnedOn(final String switchName, final SwitchParameter<?>... parameters ) {
-		
-		LocalDateTime currentDateTime = LocalDateTime.now();
-		return turnedOn(switchName, currentDateTime, parameters);
-	}
-	
-	private boolean turnedOn(final String switchName, final LocalDateTime currentDateTime, final SwitchParameter<?>... parameters ) {
-		
-		Optional<Switch> $switch = switches.stream().filter(s -> switchName.equals(s.name())).findFirst();
-		if (!$switch.isPresent()) return false;
-		
-		//any one dependency must be turned on
-		if (!activeDependency($switch.get(), currentDateTime, parameters) ) return false;
-		
-		//no one alternative must be turned on
-		if (activeAlternative($switch.get(), currentDateTime, parameters) ) return false;
-		
-		return $switch.get().active(currentDateTime, parameters );
-	}
-	
-	private boolean activeDependency(Switch $switch, final LocalDateTime currentDateTime, final SwitchParameter<?>... parameters) {
-	
-		Set<String> dependenciesNames = $switch.dependencies();
-		if (dependenciesNames.isEmpty() ) return true;
-		return dependenciesNames.stream().anyMatch(d -> turnedOn(d, currentDateTime, parameters) );
-	}
-	
-	private boolean activeAlternative(Switch $switch, final LocalDateTime currentDateTime, final SwitchParameter<?>... parameters) {
-		
-		Set<String> alternativeNames = $switch.alternatives();
-		if (alternativeNames.isEmpty() ) return false;
-		return alternativeNames.stream().anyMatch(a -> turnedOn(a, currentDateTime, parameters));
-	}
+	boolean turnedOn(final String switchName, final SwitchParameter<?>... parameters );
 }
